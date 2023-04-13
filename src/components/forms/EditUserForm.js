e56867f5f4a539a5fd01a10ed86bc7e772e9f8b1
beachf1res <lisaxbarkar@gmail.com>
2023-04-13 13:14:24 +0300

    fix img

--- src/components/forms/EditUserForm.js
+++ src/components/forms/EditUserForm.js
@@ -105,6 +105,7 @@ export const EditUserForm = () => {
               <input
                 type={"file"}
                 hidden
+                accept="image/*"
                 {...register("avatar")}
                 onChange={handleAvatarChange}
               />
